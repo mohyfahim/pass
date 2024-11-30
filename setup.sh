8c8e391a74c9a52ae0456460a8c5e23e09c0227d
Mohy <fahimohy@gmail.com>
2024-11-30 13:07:37 +0330

    chore: add script to generate gpg secrets for project credentials

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@
 export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH=$PATH:$ANDROID_HOME/emulator
+
+if [[ "$1" == "enc" ]]; then
+    gpg --symmetric --armor --cipher-algo AES256 release.keystore
+    gpg --symmetric --armor --cipher-algo AES256 credentials.json
+fi
